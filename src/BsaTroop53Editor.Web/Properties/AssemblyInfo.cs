e14d183837382cc160512a19f2bde88fb24627e2
Seth Hendrick <seth@shendrick.net>
2024-02-25 13:31:26 -0500

    Added footer with version.

---
+++ src/BsaTroop53Editor.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using BsaTroop53Editor.Web;
+
+// We probably can't use reflection in Razor,
+// so make this equal to the version information
+// class.
+[assembly: AssemblyVersion( VersionInfo.Version )]
